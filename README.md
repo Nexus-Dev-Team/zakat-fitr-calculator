de2679e877898da5f82d5a2172869d502a44e26e
yousef <yousefelsise440@gmail.com>
2026-04-15 15:55:41 +0200

    save

--- README.md
+++
@@ -1,2 +0,0 @@
-# zakat-fitr-calculator
-A Java console application that calculates the required amount of food for Zakat Al-Fitr based on the number of family members and the selected food type.
--- scr/ui/MainScreen.java
+++ scr/ui/MainScreen.java
@@ -1,19 +1,17 @@
 package ui;
 
-import consle.ConsoleHelper;
 
 public class MainScreen extends Screen {
 
     @Override
     public void show() {
-        displayHeader("Zakat Fitr Calculator");
 
-        ConsoleHelper.clear();
+        
 
-        System.out.println("===========================================");
-        System.out.println("Welcome to the Zakat Fitr Calculator!");
-        System.out.println("===========================================");
+        System.out.println("============================================================================");
+        System.out.println("Welcome to the Zakat Al Fitr Calculator,brought to you by the Nexus Team.");
+        System.out.println("============================================================================");
         System.out.println("\nPress Enter to Start...");
-        ConsoleHelper.pause();
+        
     }
 }
