714dafd886f6dc21e333af32977a06ce5cf45895
AdamHassan-Eldeso <adamhassaneldesoky@gmail.com>
2026-04-17 22:35:55 +0200

    Update FoodData.java

--- FoodData.java
+++ FoodData.java
@@ -4,13 +4,11 @@ import java.util.Locale.Category;
 public class FoodData {
     private  ArrayList<Category> foodList; 
     
-    public FoodData(){
     foodList = new ArrayList<>();
     foodList.add(new Category("Rice" , 2.3));
     foodList.add(new Category("Flour" , 2.0));
     foodList.add(new Category("Lentils" , 2.1));
     foodList.add(new Category("Wheat" , 2.040));
-    }
 
     private  ArrayList<Category> getFoodData(){
         return foodList;
