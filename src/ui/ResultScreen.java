6e9723efb820fb37201e2fb061d156a270d5e87f
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-19 10:49:15 +0200

    fix some bugs on ResultScreen class

--- src/ui/ResultScreen.java
+++ src/ui/ResultScreen.java
@@ -1,26 +1,18 @@
 package ui;
-import model.Category;
-public class ResultScreen extends screen{
 
-    public void show(){
-        headerDisplay();
-        System.out.println("◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼︎");
-        System.out.println("◼•"+"          Your inputs     "+   "•◼");
-        System.out.println("◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼︎");
-        System.out.println("food type:" +selectedCategory.getname());
-        System.out.println("Amount of each person:" +selectedCategory.getkgPerPerson());
-        System.out.println("\n\n∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷\n\n︎");
-        System.out.println("◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼︎");
-        System.out.println("◼•"+"          Your Result     "+  "•◼");
-        System.out.println("◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼•◼︎");
-        System.out.println("The total amount of the family:"+ totalKG +"Kg");
-        System.out.println("\n\n∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷∷\n\n︎");
+public class ResultScreen extends Screen {
+
+    public void show() {
+        displayHeader("Your Results");
+        System.out.println("Oops! No results to show, try again please . . .\n");
+    }
+
+    public void show(int totalKG) {
+        displayHeader("Your Results");
+        System.out.println("The total amount of the family: " + totalKG + "Kg");
         System.out.println("● Thanks for using our program︎");
         System.out.println("● With our team greetings"); 
         System.out.println("● ©︎opy®︎ight is reserved 2026 ");
         System.out.println("● By Nexus ™︎ ");
-
-        
-    }
     }
 }
